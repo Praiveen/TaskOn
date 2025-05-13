021f3540de09ffb28917f398e27d39cc14e78416
Ivan_Karagachev <ivan.karagachev@gmail.com>
2025-05-13 14:59:37 +0300

    main

--- taskon/settings.py
+++ taskon/settings.py
@@ -191,11 +191,11 @@ SIMPLE_JWT = {
 }
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "taskon-bg0x.onrender.com",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "taskon-bg0x.onrender.com",
+# ]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
